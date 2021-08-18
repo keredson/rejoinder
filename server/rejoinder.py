0c6c0a548ace6d09f992ac9f8893f644814cbc42
Derek Anderson <public@kered.org>
2021-08-18 15:51:27 -0700

    some emojis are > 2

--- server/rejoinder.py
+++ server/rejoinder.py
@@ -29,7 +29,7 @@ class Meeting:
     return len(self.users)
 
   async def send(self, frm, s):
-    if len(s) not in (1,2): return # not a valid msg
+    if len(s) > 4: return # not a valid emoji
     self.touch()
     msg = json.dumps({
       'name': frm.name,
